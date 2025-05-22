87b2d33cb9fbeed55f2aae9460db3955344204a6
bajajanirudh <aniruddhbajaj@gmail.com>
2025-05-22 12:48:17 +0530

    Add 0xacc95afa65768aa74044e6f6e267ad6417cd3e55

---
+++ official-communities/0xacc95afa65768aa74044e6f6e267ad6417cd3e55/config.py
@@ -0,0 +1,9 @@
+CONTRACT_ADDRESS='0xacc95afa65768aa74044e6f6e267ad6417cd3e55'
+PAIR_ADDRESS='0x12b6c33d628bd454b19e30c625276a62cfd89560'
+HANDLE='boithebear'
+NAME='Boi the Bear'
+TICKER='BOI'
+DESCRIPTION='One paw step at a time.'
+OWNER_HANDLE='hakikiceo'
+TOTAL_SUPPLY=1e9 * 1e18 # 1 billion
+POSTING_THRESHOLD=1e6 # 1 million
