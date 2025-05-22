50041b52b0c38a1e4e81e0d2b79533c5088859c4
bajajanirudh <aniruddhbajaj@gmail.com>
2025-05-22 12:37:32 +0530

    Add 0xcac4904e1db1589aa17a2ec742f5a6bcf4c4d037

---
+++ official-communities/0xcac4904e1db1589aa17a2ec742f5a6bcf4c4d037/config.py
@@ -0,0 +1,9 @@
+CONTRACT_ADDRESS='0xcac4904e1db1589aa17a2ec742f5a6bcf4c4d037'
+PAIR_ADDRESS='0xf02d008e707755b4d855b201fd635dc56de327b3'
+HANDLE='ErolMusk'
+NAME='EROL Musk'
+TICKER='EROL'
+DESCRIPTION='Erol Musk is a community-driven memecoin built around a fictional character on a mission to build a new life on Mars.'
+OWNER_HANDLE='BenBirTrolum'
+TOTAL_SUPPLY=1e9 * 1e18 # 1 billion
+POSTING_THRESHOLD=1e5 # 100,000
